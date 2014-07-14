fb2694caff309ff274ad0d40b316ff6b593aeb94
alan <ggddll123@qq.com>
2014-07-14 23:01:47 +0800

    样式更换

--- release/magicform-0.0.1.js
+++ release/magicform-0.0.1.js
@@ -1,5 +1,348 @@
 /**
- * 配置引擎，实现通用化的配置解析与还原
+ * 魔法表单，让表单从此轻松
+ * @project
+ * @name MagicFrom
+ * @subtitle v0.0.1
+ * @download http://115.29.195.88:92/release/magicform-0.0.1.js
+ * @support ie,chrome,firefox
+ * @howto
+ * 首先假设已经在html页面中拥有一个form表单，假设结构如下：
+
+        <form class="mf" style="width:600px;margin:10% auto;" action="javascript:;">
+            <fieldset class="lightgraybg2">
+                <legend>Register</legend>
+                <div id="form"></div>
+            </fieldset>
+        </form>
+
+    现有如下json数据，希望自动生成表单并填充到上面的form中
+
+        //假设现在有一个json数据如下
+        var datajson = {
+            email:"",
+            username:"",
+            password:"",
+            message:""
+        };
+        
+        //重点来啦，完全自动化生成
+        var form = document.getElementById("form");
+        magicform.generate(form,datajson,{
+            status:"editable"
+        });
+            
+
+    ## 设置
+
+    在生成表单的时候，你可以进行简单的设置，就像这样
+
+        magicform.generate(form,datajson,{
+            status:"editable"
+        });
+        
+    这里简单说明一下每个字段的含义和取值
+
+    - **status**
+
+        表单的状态，取值可以是 "disabled"——只读的,"editable"——允许变更的
+
+    - **style**
+
+        给表单设置自定义样式
+
+    - **classname**
+
+        给表单添加class
+        
+        例如：
+        
+            classname:{
+            
+                //label是文本标签的样式名，control是控件的样式，magicform内置了许多classname来组合出各种风格的表单
+                //当然你可以自己重写这些样式
+                
+                label:"w20p align-right mr10p",
+                control:"w70p"
+            }
+        
+        
+    - **functions**
+
+
+
+    ## 数据结构和生成表单的格式说明
+    magicform支持多种类型的数据，根据不同类型来生成不同的html结构，满足多种需求，json数据分为两种
+        
+        {a:1}
+        {a:{mf:1,value:1}}
+        
+    第一种结构只有json的key值和value值，这种结构将直接生成input标签并且类型是string，最通用的形式，但如果你想要对表单控件样式加以控制，就需要用到第二种结构，第二种结构json的key值a对应的是一个object，这个object中有个mf:1，表示这是个包含mf配置数据的结构，magicform将会读取配置信息对form控件的样式进行控制，object中的value字段才是真正的值，关于配置字段的详解参见第三节。下面是一个最简单的数据类型和他对应生成的html结构的对照表。
+
+
+        ** json **
+
+            {a:1,b:2}
+
+        ** html **
+        
+            <!--省略部分class-->
+            
+            <p><lable>a</lable><input/></p>
+            <p><lable>b</lable><input/></p>
+            
+            <!--value是input string，转换成json时会有特殊处理-->
+            <!--this表示元素自己-->
+            <p><lable>a</lable><input data-mf-exp="this.value*2"/></p>
+            
+            
+    ## 配置字段说明
+
+    magicform允许对生成的表单元素进行丰富的定制，比如
+
+        name:{mf:1,control:"text",validation:"required maxlength=10 minlength=4",label:"姓名",value:""}
+
+    这段配置定制的name字段的控件类型，以及label标签的名称，还包括验证相关的规则。
+
+    下面详细说明每个配置字段的含义 
+
+    - **mf:1**
+
+        这个字段一直为1，标明这个object是mf的配置结构。
+        
+    - **control**
+
+        配置控件的类型，取值可以是text,password,checkbox,radio,date,week等值。
+        
+    - **value**
+
+        字段的值
+
+    - **order**
+
+        配置控件显示的顺序，数字越小越靠前，如果不写，将默认排在有order控件的后面。
+        
+    - **options** 
+
+        用于select的选项，数组形式
+        
+        例如：
+        
+            options:[{label:"初中",checked:"checked"},{label:"高中"}]
+
+    - **name**
+
+        用于checkbox或radio中的配对
+            
+
+    - **hash**
+
+        这个为1表示这是一个hash类型的数据
+
+    - **fornew**
+
+        这个字段为1表示这个配置是用来新增数据时用的
+
+    - **key**
+
+        用于hash类型数据，填写一个对象用来配置hash类型中key的属性，配置的格式和本节介绍的配置字段相同
+
+    - **validation**
+        
+        数据验证的规则，比如：
+        
+            password:{mf:1,control:"password",validation:"minlength=6 required"}
+            email:{mf:1,control:"text",validation:"required pattern='.*@.*'"}
+
+    - **label**
+
+        标签文本
+
+    - **placeholder**
+
+        占位符 
+        
+    - **attaproc**
+
+        这个字段传入一个function，可以自由定义如何把值显示出来
+        
+        例如：
+        
+            function(atta,prop,src,index){
+            }
+
+    - **detaproc**
+
+        这个字段传入一个function，可以自由定义如何把值取出来
+        
+        例如：
+        
+            function(prop,src){
+            }
+
+    - **isobject**
+
+        这个字段为1表示该字段为一个对象
+        
+    - **onclick**
+
+        当字段为一个对象时，onclick可以传入一个function决定当点击对象详情icon时的行为
+        
+        例如：
+        
+            function(json){
+            }
+
+
+    ## 数据和配置分离
+        
+    有的时候我们需要将单纯的数据存储在后台，然后显示的时候再赋予更多样式和效果，以及一些校验规则，这个时候我们就需要用到数据和配置分离了。magicform支持数据和配置分离，这样结构更加清晰，方便你移植以前的老表单。
+
+    例如：
+
+         //纯数据
+        var pureJson = {
+            email: "",
+            username: "",
+            password: "",
+            message: ""
+        };
+
+         //attach数据
+        var jsonObj = {
+            email: {
+                mf: 1,
+                control: "text",
+                validation: "required pattern='.*@.*'"
+            },
+            password: {
+                mf: 1,
+                control: "password",
+                validation: "minlength=6 required"
+            },
+            username: {
+                mf: 1,
+                validation: "required maxlength=10 minlength=4"
+            },
+            message: {
+                mf: 1,
+                placeholder: "leave a short message",
+                control: "textarea"
+            }
+        };
+
+        magicform.generate(form, magicform.attach(pureJson, jsonObj), {
+            status: "editable",
+            hasbuttons: true
+        });
+            
+    magicform.attach把数据pureJson和jsonObj和在一起返回一个新的结构用来生成表单，默认的合并方法仅仅是将数据的值，给到配置字段的value，如果你的机构有些复杂，也可以自定义一个attaproc
+
+    例如：
+
+         //hashattachProc
+         //atta是属性的配置，prop是属性名，src是源数据，index是数组的下标（如果是数组的话)
+        var hashAttachProc = function(atta, prop, src, index) {
+            atta.value = src[prop][index].value;
+            atta.key.value = src[prop][index].key;
+
+            return atta;
+        };
+
+         //配置摘要
+         {
+                ...
+            返回码: [{
+                mf: 1,
+                hash: 1,
+                fornew: 1,
+                control: "text",
+                key: {
+                    mf: 1,
+                    control: "text"
+                }
+            }, {
+                mf: 1,
+                hash: 1,
+                control: "text",
+                key: {
+                    mf: 1,
+                    control: "text"
+                },
+                attaproc: hashAttachProc,
+                detaproc: hashDetachProc
+            }, {
+                mf: 1,
+                hash: 1,
+                control: "text",
+                key: {
+                    mf: 1,
+                    control: "text"
+                },
+                attaproc: hashAttachProc,
+                detaproc: hashDetachProc
+            }]
+                ...
+        }
+
+
+        magicform.generate(form, magicform.attach(pureJson, jsonObj));
+        
+    同样，你也可以在表单填写完成的时候把数据分离成单纯的数据文件用来持久化，这个时候你需要用到方法
+
+    magicform.detach，同时你也可以自定义一个detaproc来自定义分离的逻辑
+
+    例如：
+        
+         //hashdetachProc
+        var hashDetachProc = function(prop, src) {
+            var obj = {};
+            obj.value = src.value;
+            obj.key = src.key.value;
+
+            return obj;
+        }
+
+         //配置摘要
+         {
+                ...
+            返回码: [{
+                mf: 1,
+                hash: 1,
+                fornew: 1,
+                control: "text",
+                key: {
+                    mf: 1,
+                    control: "text"
+                }
+            }, {
+                mf: 1,
+                hash: 1,
+                control: "text",
+                key: {
+                    mf: 1,
+                    control: "text"
+                },
+                attaproc: hashAttachProc,
+                detaproc: hashDetachProc
+            }, {
+                mf: 1,
+                hash: 1,
+                control: "text",
+                key: {
+                    mf: 1,
+                    control: "text"
+                },
+                attaproc: hashAttachProc,
+                detaproc: hashDetachProc
+            }]
+                ...
+        }
+
+         //jsonObj是字段配置对象    
+        JSON.stringify(magicform.detach(magicform.html2json(form), jsonObj));
+ *
+ *
+ *
  * @author alan
  * 2013/12/12
  */
@@ -81,7 +424,11 @@
     })();
 
 
-    /* exported configEngine */
+    /* exported magicForm */
+    /**
+     * 魔法表单主类
+     * @class magicForm
+     */
     var magicForm = (function() {
         return {
             formTemplate: template.formTemplate,
@@ -100,6 +447,8 @@
             /**
              * @method _setOptions
              * @private
+             * @for magicForm
+             * @support: ie:>=10,chrome:all,firefox:all
              */
             _setOptions: function(options) {
                 options = options || {};
@@ -126,11 +475,13 @@
             /**
              * json2html -> init
              * @method generate
-             * @param json 数据对象
-             * @param options
+             * @for magicForm
+             * @param json {Object} 数据对象
+             * @param options {Object}
              * @param options.status {String} 表单状态 "disabled","editable","new"
              * @param options.functions {Array} 表单功能 ["editable"]
              * @param options.buttons {Array} 按钮功能 [{name:"submit","title":"提交表单"},{name:"cancel",title:""},{name:"reset"}]
+             * @support: ie:>=10,chrome:all,firefox:all
              */
 
             generate: function(form, json, options) {
@@ -140,13 +491,15 @@
             },
 
             /**
-             * json -> html
+             * json -> html json对象转换成html表单，提供配置，可以配置样式和状态
              * @method json2html
-             * @param json 数据对象
-             * @param options
+             * @for magicForm
+             * @param json {Object} 数据对象
+             * @param options {Object} 配置
              * @param options.status {String} 表单状态 "disabled","editable","new"
              * @param options.functions {Array} 表单功能 ["editable"]
              * @param options.buttons {Array} 按钮功能 [{name:"submit","title":"提交表单"},{name:"cancel",title:""},{name:"reset"}]
+             * @support: ie:>=10,chrome:all,firefox:all
              */
             json2html: function(json, options) {
                 var orders = [];
@@ -179,11 +532,32 @@
             /**
              * 把显示属性，附加上去，此方法可以保持数据对象的纯净
              * @method attach
-             * @param attaproc
-             * @param attaproc.atta 附加对象
-             * @param attaproc.prop 属性
-             * @param attaproc.src 源数据
-             * @param attaproc.index 索引
+             * @for magicForm
+             * @param json {Object} 数据
+             * @param attr {Object} 属性
+             * @param attr.mf {Number} 这个字段一直为1，标明这个object是mf的配置结构
+             * @param attr.control {String} 配置控件的类型，取值可以是text,password,checkbox,radio,date,week等值
+             * @param attr.value {Object} 字段的值
+             * @param attr.order {Number} 字段显示顺序
+             * @param attr.options {Array} 用于select的选项，数组形式，例如：options:[{label:"初中",checked:"checked"},{label:"高中"}]
+             * @param attr.name {String} 用于checkbox或radio中的配对
+             * @param attr.hash {Number} 这个为1表示这是一个hash类型的数据
+             * @param attr.fornew {Number} 这个字段为1表示这个配置是用来新增数据时用的
+             * @param attr.key {Object} 用于hash类型数据，填写一个对象用来配置hash类型中key的属性，配置的格式和本节介绍的配置字段相同
+             * @param attr.validation {String} 数据验证的规则，比如：password:{mf:1,control:"password",validation:"minlength=6 required"}，email:{mf:1,control:"text",validation:"required pattern='.*@.*'"}
+             * @param attr.label {String} 标签文本
+             * @param attr.placeholder {String} 占位符
+             * @param attr.attaproc {Function} 自定义属性显示方法
+             *  @param attr.attaproc.atta {String} 附加对象
+             *  @param attr.attaproc.prop {String} 属性
+             *  @param attr.attaproc.src {String} 源数据
+             *  @param attr.attaproc.index {Number} 索引
+             * @param attr.detaproc 自定义数据提取方法
+             *  @param attr.detaproc.prop {String} 属性
+             *  @param attr.detaproc.src {String} 源数据
+             * @param attr.isobject {Number} 这个字段为1表示该字段为一个对象
+             * @param attr.onclick {Function} 当字段为一个对象时，onclick可以传入一个function决定当点击对象详情icon时的行为例如：function(json){}
+             * @support: ie:>=10,chrome:all,firefox:all
              */
 
             attach: function(json, attr) {
@@ -217,7 +591,7 @@
                 };
 
                 for (var p in jsonCopy) {
-                    if (attr[p]) {
+                    if (attr[p] !== undefined) {
                         var index = 0;
                         if (/array/i.test(util._typeof(attr[p]))) {
                             for (var i = 0; i < attr[p].length; i++) {
@@ -241,10 +615,32 @@
             /**
              * 把显示属性，剥离出来，此方法可以保持数据对象的纯净
              * @method detach
-             * @param detaproc
-             * @param detaproc.prop 属性
-             * @param detaproc.src 源数据
-             * @param detaproc.index 索引
+             * @for magicForm
+             * @param json {Object} 数据
+             * @param attr {Object} 属性
+             * @param attr.mf {Number} 这个字段一直为1，标明这个object是mf的配置结构
+             * @param attr.control {String} 配置控件的类型，取值可以是text,password,checkbox,radio,date,week等值
+             * @param attr.value {Object} 字段的值
+             * @param attr.order {Number} 字段显示顺序
+             * @param attr.options {Array} 用于select的选项，数组形式，例如：options:[{label:"初中",checked:"checked"},{label:"高中"}]
+             * @param attr.name {String} 用于checkbox或radio中的配对
+             * @param attr.hash {Number} 这个为1表示这是一个hash类型的数据
+             * @param attr.fornew {Number} 这个字段为1表示这个配置是用来新增数据时用的
+             * @param attr.key {Object} 用于hash类型数据，填写一个对象用来配置hash类型中key的属性，配置的格式和本节介绍的配置字段相同
+             * @param attr.validation {String} 数据验证的规则，比如：password:{mf:1,control:"password",validation:"minlength=6 required"}，email:{mf:1,control:"text",validation:"required pattern='.*@.*'"}
+             * @param attr.label {String} 标签文本
+             * @param attr.placeholder {String} 占位符
+             * @param attr.attaproc {Function} 自定义属性显示方法
+             *  @param attr.attaproc.atta {String} 附加对象
+             *  @param attr.attaproc.prop {String} 属性
+             *  @param attr.attaproc.src {String} 源数据
+             *  @param attr.attaproc.index {Number} 索引
+             * @param attr.detaproc 自定义数据提取方法
+             *  @param attr.detaproc.prop {String} 属性
+             *  @param attr.detaproc.src {String} 源数据
+             * @param attr.isobject {Number} 这个字段为1表示该字段为一个对象
+             * @param attr.onclick {Function} 当字段为一个对象时，onclick可以传入一个function决定当点击对象详情icon时的行为例如：function(json){}
+             * @support: ie:>=10,chrome:all,firefox:all
              */
 
             detach: function(json, attr) {
@@ -289,7 +685,9 @@
             /**
              * html2value
              * @method _html2value
+             * @for magicForm
              * @private
+             * @support: ie:>=10,chrome:all,firefox:all
              */
             _html2value: function(control) {
                 var json = null;
@@ -419,8 +817,12 @@
                 return json;
             },
             /**
-             * html -> json
+             * html -> json 从html表单中提取数据对象
              * @method html2json
+             * @for magicForm
+             * @param form {HTMLElement} 表单元素
+             * @param options {Object} 设置选项
+             * @support: ie:>=10,chrome:all,firefox:all
              */
             html2json: function(form, options) {
                 options = options || {};
@@ -438,9 +840,13 @@
             /**
              * 初始化一些事件
              * @method init
+             * @for magicForm
+             * @param wrapper {HTMLElement} 表单元素，一般是from，但也可以是div等其他元素
+             * @param json {Object} 数据对象
+             * @param options {Object} 设置
+             * @support: ie:>=10,chrome:all,firefox:all
              */
             init: function(wrapper, json, options) {
-                var self = this;
                 options = this._setOptions(options);
                 var initRemoveAnchor = function(removeAnchor) {
                     removeAnchor.addEventListener("click", function() {
